e0bf8cb98dedef6aac08a4b4a6d503ebe73d88b6
huhuiyu <1069306849@qq.com>
2023-06-25 11:18:59 +0800

    多仓库推送脚本

---
+++ push.ps1
@@ -0,0 +1,7 @@
+# git@github.com:huhuiyu/template-springboot2.git
+# git remote add jihulab git@jihulab.com:DarkKnight/template-springboot2.git
+# git remote add gitee git@gitee.com:huhuiyu/template-springboot2.git
+git pull
+git push
+git push jihulab
+git push gitee
