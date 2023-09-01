5e4d394dae396cb0bd4124ef8d3bcb31feddc489
huhuiyu <1069306849@qq.com>
2023-09-01 14:45:29 +0800

    maven打包脚本和应用监听器配置指定端口启动浏览器

---
+++ maven-springboot2/build.ps1
@@ -0,0 +1,3 @@
+..\scripts\java-home.ps1
+..\scripts\maven-home.ps1
+mvn clean package -DskipTests=true
--- maven-springboot2/src/main/java/top/huhuiyu/template/maven/springboot2/listener/ApplicationReadyListener.java
+++ maven-springboot2/src/main/java/top/huhuiyu/template/maven/springboot2/listener/ApplicationReadyListener.java
@@ -2,9 +2,13 @@ package top.huhuiyu.template.maven.springboot2.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
+
+import java.awt.*;
+import java.net.URI;
 
 /**
  * 监听应用程序启动
@@ -13,11 +17,21 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ApplicationReadyListener implements ApplicationListener<ApplicationReadyEvent> {
+  @Value("${server.port}")
+  private String port;
 
   private static final Logger log = LoggerFactory.getLogger(ApplicationReadyListener.class);
 
   @Override
   public void onApplicationEvent(ApplicationReadyEvent event) {
+    if("10100".equals(port)) {
+      System.setProperty("java.awt.headless", "false");
+      try {
+        Desktop.getDesktop().browse(new URI(String.format("http://127.0.0.1:%s?echo=DarkKnight", port)));
+      } catch (Exception e) {
+        log.error("无法启动浏览器",e);
+      }
+    }
     log.info("应用程序启动完成");
   }
 }
